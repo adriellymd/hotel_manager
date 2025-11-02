e9ab494f90e946263e5d9cdfac0019df0b9f07a4
wilianne <wwilliannequaresmapaixao@gmail.com>
2025-11-02 20:32:43 -0300

    Entidades

--- hotel_manager/entidades.hpp
+++ hotel_manager/entidades.hpp
@@ -39,4 +39,54 @@ public:
     Senha get_senha() const; /**< retorna a senha */
 };
 
+/**
+ * @class hotel
+ * @brief representa um hotel do sistema.
+ */
+class Hotel {
+private:
+    Nome nome; /**< nome do hotel */
+    Endereco endereco; /**< endereco do hotel */
+    Telefone telefone; /**< telefone do hotel */
+    Codigo codigo; /**< codigo do hotel */
+public:
+    Hotel() = default;
+    Hotel(const Nome &n, const Endereco &e,const Telefone &t,const Codigo &c); /**< construtor com atributos */
+
+    void set_nome(const Nome &n); /**< define o nome */
+    Nome get_nome() const; /**< retorna o nome */
+    void set_endereco(const Endereco &e); /**< define o endereco */
+    Endereco get_endereco() const; /**< retorna o endereco */
+    void set_telefone(const Telefone &t); /**< define o telefone*/
+    Telefone get_telefone() const; /**< retorna o endereco*/
+    void set_codigo(const Codigo &c); /**< define o codigo*/
+    Codigo get_codigo() const; /**< retorna o codigo*/
+
+};
+
+/**
+ * @class quarto
+ * @brief representa um quarto do sistema.
+ */
+class Quarto {
+private:
+    Numero numero; /**< numero do quarto */
+    Capacidade capacidade; /**< capacidade do quarto */
+    Dinheiro diaria; /**< diaria do quarto */
+    Ramal ramal; /**< ramal do quarto */
+public:
+    Quarto() = default;
+    Quarto(const Numero &n, const Capacidade &c,const Dinheiro &d,const Ramal &r); /**< construtor com atributos */
+
+    void set_numero(const Numero &n); /**< define o numero */
+    Numero get_numero() const; /**< retorna o numero */
+    void set_capacidade(const Capacidade &c); /**< define a capacidade */
+    Capacidade get_capacidade() const; /**< retorna a capacidade */
+    void set_diaria(const Dinheiro &d); /**< define a diaria*/
+    Dinheiro get_diaria() const; /**< retorna a diaria*/
+    void set_ramal(const Ramal &r); /**< define o ramal*/
+    Ramal get_ramal() const; /**< retorna o ramal*/
+
+};
+
 #endif // ENTIDADES_HPP_INCLUDED
